e5972d802d0c24ac5a68df7054661935e217b712
MartinGebske <martin.gebske@hotmail.de>
2026-02-14 12:13:16 +0100

    Basic Movement on Bars

--- audio_spectrum.gd
+++ audio_spectrum.gd
@@ -4,11 +4,11 @@ const BAR_COUNT: int = 32 # minimum is 8
 const FREQ_MAX: float = 11050.0
 const MIN_DB: int = 60
 
-var cubes: Array[MeshInstance3D] = []
-
 var spectrum = AudioEffectSpectrumAnalyzerInstance
 var heights: Array[Height] = []
-var bar_width: float = 0.0 # perhaps this is my bar height...
+var cubes: Array = []
+
+var bar_scene: PackedScene = preload("res://scenes/bar.tscn")
 
 @onready var cube: MeshInstance3D = $cube
 
@@ -17,11 +17,14 @@ func _ready() -> void:
 	spectrum = AudioServer.get_bus_effect_instance(0, 0)
 	for i in BAR_COUNT:
 		heights.append(Height.new())
-		var new_cube = cube.duplicate()  # Klone den Cube
-		new_cube.position.x = i * 1.2  # Positioniere nebeneinander mit Abstand
-		new_cube.position.z = 0 # Alle auf die selbe Z achse
-		add_child(new_cube)  # Füge zur Szene hinzu
-		cubes.append(new_cube)  # Speichere im Array
+		var new_bar = bar_scene.instantiate()
+		new_bar.position.x = i * 5.0  # Positioniere nebeneinander mit Abstand
+		new_bar.scale.x = 1.5
+		new_bar.scale.z = 1.5
+		add_child(new_bar)
+		new_bar.position.z = 0 # Alle auf die selbe Z achse
+		cubes.append(new_bar)
+
 		cube.queue_free()
 
 
@@ -31,13 +34,17 @@ func _process(_delta: float) -> void:
 
 func display_something() -> void:
 	for i in BAR_COUNT:
-		#var l_color = Color.from_hsv((BAR_COUNT * 0.6 + i * 0.5) / BAR_COUNT, 0.5, 0.6)
-		# Rainbow:
-		var l_color = Color.from_hsv(float(i) / BAR_COUNT, 0.9, 0.8)
-		cubes[i].scale.y = heights[i].actual * 4 # TODO: Magic number Setze die Höhe für jeden Cube
+		var hue = float(i) / float(BAR_COUNT - 1)
+		var l_color = Color.from_hsv(hue, 1.0, 1.0)
+
+		cubes[i].scale.y = max(heights[i].actual * 10, 0.01) # TODO: Magic number Setze die Höhe für jeden Cube
 		cubes[i].position.y = cubes[i].scale.y / 2
-		cubes[i].material_override = StandardMaterial3D.new()
-		cubes[i].material_override.albedo_color = l_color  # Farbe zuweisen
+
+		var mesh_instance = cubes[i].get_node("MeshInstance3D")
+		var material = StandardMaterial3D.new()
+		material.albedo_color = l_color
+		mesh_instance.material_override = material
+
 
 func _update_spectrum_data() -> void:
 	var l_prev_hz: float = 0.0
@@ -53,7 +60,8 @@ func _update_spectrum_data() -> void:
 		l_prev_hz = l_hz
 
 func _on_resized() -> void:
-	bar_width = cube.scale.x / BAR_COUNT # 1000 pro falsch
+	pass
+	#bar_width = cube.scale.x / BAR_COUNT # 1000 pro falsch
 
 
 
